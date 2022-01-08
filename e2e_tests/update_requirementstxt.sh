ab1892cbeb70c1f1a7db2e32db78cfc729b8379d
Hannes Körber <hannes@hkoerber.de>
2022-01-08 14:21:19 +0100

    pip-update: Never write pkg_resources to requirements.txt

--- e2e_tests/update_requirementstxt.sh
+++ e2e_tests/update_requirementstxt.sh
@@ -12,7 +12,7 @@ pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | while re
     pip install --upgrade "${package}"
     version="$(pip show "${package}" | grep '^Version' | cut -d ' ' -f 2)"
     message="e2e_tests/pip: Update ${package} to ${version}"
-    pip freeze > requirements.txt
+    pip freeze | grep -v '^pkg_resources' > requirements.txt
     git add ./requirements.txt
     git commit --message "${message}"
 done
