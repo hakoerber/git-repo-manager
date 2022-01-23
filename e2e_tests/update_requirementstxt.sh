f8adec14133f31f1336e282ad2a4b5646f2f6138
Hannes Körber <hannes@hkoerber.de>
2022-01-23 12:27:59 +0100

    e2e: Ignore pip and setuptools for autoupdate

--- e2e_tests/update_requirementstxt.sh
+++ e2e_tests/update_requirementstxt.sh
@@ -9,6 +9,8 @@ source ./venv/bin/activate
 pip --disable-pip-version-check install -r ./requirements.txt
 
 pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | while read -r package ; do
+    [[ package == "pip" ]] && continue
+    [[ package == "setuptools" ]] && continue
     pip install --upgrade "${package}"
     version="$(pip show "${package}" | grep '^Version' | cut -d ' ' -f 2)"
     message="e2e_tests/pip: Update ${package} to ${version}"
