ca64f5473e8449ff8b5241c1f17fd3556fd65d22
Hannes Körber <hannes@hkoerber.de>
2024-04-27 10:09:04 +0200

    Replace tempdir with outdir-tempdir crate

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -1,11 +1,11 @@
-use tempdir::TempDir;
+use outdir_tempdir::TempDir;
 
 pub fn init_tmpdir() -> TempDir {
-    let tmp_dir = TempDir::new("grm-test").unwrap();
+    let tmp_dir = TempDir::new().autorm();
     println!("Temporary directory: {}", tmp_dir.path().display());
     tmp_dir
 }
 
-pub fn cleanup_tmpdir(tempdir: TempDir) {
-    tempdir.close().unwrap();
+pub fn cleanup_tmpdir(_tempdir: TempDir) {
+    // cleanup on drop
 }
