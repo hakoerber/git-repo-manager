f93c52b4b6fcec0261700870f698621c7b299bc5
Sirio Balmelli <sirio@b-ad.ch>
2024-08-22 10:02:40 +0200

    nix: update sources
    Pin to tip of latest release 24.05.
    
    Check build.
    
    Signed-off-by: Sirio Balmelli <sirio@b-ad.ch>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "git-repo-manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     crane = {
@@ -12,7 +12,6 @@
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
