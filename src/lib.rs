077292b94d704b4f11e21b46a4acaffa1742bcb7
Hannes Körber <hannes@hkoerber.de>
2025-07-23 22:15:46 +0200

    Refactor tree traversal

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![forbid(unsafe_code)]
 
-use std::path::Path;
+use std::{fmt::Display, path::Path};
 
 use thiserror::Error;
 
@@ -26,15 +26,26 @@ pub enum Error {
     CannotDetectRootDirectory,
 }
 
+pub struct Warning(String);
+
+impl Display for Warning {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+struct FindResult {
+    repos: Repos,
+    warnings: Vec<Warning>,
+}
+
+enum Repos {
+    InSearchRoot(repo::Repo),
+    List(Vec<repo::Repo>),
+}
+
 /// Find all git repositories under root, recursively
-///
-/// The bool in the return value specifies whether there is a repository
-/// in root itself.
-#[allow(clippy::type_complexity)]
-fn find_repos(
-    root: &Path,
-    exclusion_pattern: Option<&str>,
-) -> Result<Option<(Vec<repo::Repo>, Vec<String>, bool)>, Error> {
+fn find_repos(root: &Path, exclusion_pattern: Option<&str>) -> Result<FindResult, Error> {
     let mut repos: Vec<repo::Repo> = Vec::new();
     let mut repo_in_root = false;
     let mut warnings = Vec::new();
@@ -45,7 +56,10 @@ fn find_repos(
         })?;
     for path in tree::find_repo_paths(root)? {
         if exclusion_pattern.is_some() && exlusion_regex.is_match(&path::path_as_string(&path)) {
-            warnings.push(format!("[skipped] {}", &path::path_as_string(&path)));
+            warnings.push(Warning(format!(
+                "[skipped] {}",
+                &path::path_as_string(&path)
+            )));
             continue;
         }
 
@@ -56,23 +70,23 @@ fn find_repos(
 
         match repo::RepoHandle::open(&path, is_worktree) {
             Err(error) => {
-                warnings.push(format!(
+                warnings.push(Warning(format!(
                     "Error opening repo {}{}: {}",
                     path.display(),
                     if is_worktree { " as worktree" } else { "" },
                     error
-                ));
+                )));
                 continue;
             }
             Ok(repo) => {
                 let remotes = match repo.remotes() {
                     Ok(remote) => remote,
                     Err(error) => {
-                        warnings.push(format!(
+                        warnings.push(Warning(format!(
                             "{}: Error getting remotes: {}",
                             &path::path_as_string(&path),
                             error
-                        ));
+                        )));
                         continue;
                     }
                 };
@@ -86,12 +100,12 @@ fn find_repos(
                             let remote_type = match repo::detect_remote_type(&url) {
                                 Ok(t) => t,
                                 Err(e) => {
-                                    warnings.push(format!(
+                                    warnings.push(Warning(format!(
                                         "{}: Could not handle URL {}. Reason: {}",
                                         &path::path_as_string(&path),
                                         &url,
                                         e
-                                    ));
+                                    )));
                                     continue;
                                 }
                             };
@@ -103,11 +117,11 @@ fn find_repos(
                             });
                         }
                         None => {
-                            warnings.push(format!(
+                            warnings.push(Warning(format!(
                                 "{}: Remote {} not found",
                                 &path::path_as_string(&path),
                                 remote_name
-                            ));
+                            )));
                             continue;
                         }
                     };
@@ -122,7 +136,7 @@ fn find_repos(
                                 path::path_as_string(path.strip_prefix(parent).unwrap())
                             }
                             None => {
-                                warnings.push(String::from("Getting name of the search root failed. Do you have a git repository in \"/\"?"));
+                                warnings.push(Warning(String::from("Getting name of the search root failed. Do you have a git repository in \"/\"?")));
                                 continue;
                             }
                         },
@@ -149,33 +163,39 @@ fn find_repos(
             }
         }
     }
-    Ok(Some((repos, warnings, repo_in_root)))
+    Ok(FindResult {
+        repos: if repo_in_root {
+            Repos::InSearchRoot(if repos.len() != 1 {
+                panic!("found multiple repos in root?")
+            } else {
+                repos.pop().unwrap()
+            })
+        } else {
+            Repos::List(repos)
+        },
+        warnings,
+    })
 }
 
 pub fn find_in_tree(
     path: &Path,
     exclusion_pattern: Option<&str>,
-) -> Result<(tree::Tree, Vec<String>), Error> {
+) -> Result<(tree::Tree, Vec<Warning>), Error> {
     let mut warnings = Vec::new();
 
-    let (repos, repo_in_root): (Vec<repo::Repo>, bool) = match find_repos(path, exclusion_pattern)?
-    {
-        Some((vec, mut repo_warnings, repo_in_root)) => {
-            warnings.append(&mut repo_warnings);
-            (vec, repo_in_root)
-        }
-        None => (Vec::new(), false),
-    };
+    let mut result = find_repos(path, exclusion_pattern)?;
 
-    let mut root = path.to_path_buf();
-    if repo_in_root {
-        root = match root.parent() {
-            Some(root) => root.to_path_buf(),
-            None => {
-                return Err(Error::CannotDetectRootDirectory);
-            }
-        }
-    }
+    warnings.append(&mut result.warnings);
+
+    let (root, repos) = match result.repos {
+        Repos::InSearchRoot(repo) => (
+            path.parent()
+                .ok_or(Error::CannotDetectRootDirectory)?
+                .to_path_buf(),
+            vec![repo],
+        ),
+        Repos::List(repos) => (path.to_path_buf(), repos),
+    };
 
     Ok((
         tree::Tree {
