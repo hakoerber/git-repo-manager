38504bd8999b4daf5eafe3fc0e0a12a4a2ba2859
hakoerber <hakoerber@users.noreply.github.com>
2026-07-29 16:18:05 +0000

    deploy: c0bb3bf49d4c174b35da4fcccd05aa5982daa77f

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4fb9877e.js',
+                path_to_root + 'searchindex-d253b90b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
